423f24d3b8126d74362cda22e6859a7874dde065
soorichu <soorichu@gmail.com>
2021-08-11 19:10:32 +0900

    3D Ball Agent

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    float updown; float leftright;
+    float velocity = 0.5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updown -= Input.GetAxis("Horizontal");
+        leftright -= Input.GetAxis("Vertical");
+
+        transform.rotation = Quaternion.Euler(leftright * velocity, 0, updown * velocity);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# My3DBall
+
+using ML-Agents, Training the cube to balance the ball so that it doesn't drop it.
